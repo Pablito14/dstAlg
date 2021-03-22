19a342de376167c20addacc046a9d1c602d73a72
Pablito14 <pablo.rosales14@gmail.com>
2021-03-22 16:51:24 -0700

    Setting up repo

---
+++ ArrayReverse.java
@@ -0,0 +1,15 @@
+public class ArrayReverse {
+    public static void main(String[] args){
+        int[] testArr = {1, 2, 3, 4, 5, 6, 7};
+        int [] reversedArr;
+
+        System.out.println();
+    }
+
+    public static int[] reversedArr(int[] input){
+        int[] reversed = new int[input.lenth];
+
+        // iterate over all the array elements
+    }
+
+}
